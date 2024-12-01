c77829d23344f17ba1cc2fbdeb4006d46b501124
Jan0229 <s10270588@connect.np.edu.sg>
2024-12-01 20:43:32 +0800

    Edited the alert message

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ function booking() {
     const email = document.getElementById("email").value;
     const seating = document.getElementById("seating").value;
     const quantity = document.getElementById("quantity").value;
-    
+
     const data = {
         name: name,
         email: email,
@@ -18,5 +18,5 @@ function booking() {
         quantity: quantity
     };
 
-    alert("Booking complete, ${name}.\nYou have booked ${quantity} seats in the ${seating} section.\nAn email has been sent to ${email} with your booking details.");
+    alert("Booking complete, " + name + ".\nYou have booked " + quantity + " seats in the " + seating + " section.\nAn email has been sent to " + email + " with your booking details.");
 }
