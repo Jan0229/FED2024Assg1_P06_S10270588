3c3c4a376d5b4a9d823f8558e8ad5a56b27a374b
Jan0229 <s10270588@connect.np.edu.sg>
2024-12-01 20:16:58 +0800

    Get data when button is press

---
+++ srcipt.js
@@ -0,0 +1,6 @@
+button.addEventListener("click", () => {
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const seating = document.getElementById("seating").value;
+    const quantity = document.getElementById("quantity").value;
+});
